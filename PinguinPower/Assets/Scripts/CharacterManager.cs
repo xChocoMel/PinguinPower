85e075b1b7329f4aea23f5a55d368316ac9e374e
Alexandervc <a.vancuijk@student.fontys.nl>
2015-06-11 13:06:06 +0200

    sounds??

--- PinguinPower/Assets/Scripts/CharacterManager.cs
+++ PinguinPower/Assets/Scripts/CharacterManager.cs
@@ -7,7 +7,11 @@ public class CharacterManager : MonoBehaviour {
 
     public MenuManager menuManager;
 
+    public AudioClip collectFishClip;
+    public AudioClip penguinClip;
+
     private Animator animator;
+    private AudioSource audioSource;
 
     private int lives = 3;
     private int fish = 0;
@@ -17,6 +21,7 @@ public class CharacterManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         this.animator = this.GetComponentInChildren<Animator>();
+        this.audioSource = this.GetComponentInChildren<AudioSource>();
         StartCoroutine(InitUI());
 	}
 	public int GetLives()
@@ -95,6 +100,17 @@ public class CharacterManager : MonoBehaviour {
             case "Friend":
                 this.CollideFriend(other);
                 break;
+            case "Fall":
+                StartCoroutine(GameOver());
+                break;
+            case "Gate":
+                int newLevel = Application.loadedLevel + 1;
+                if (newLevel > 2)
+                {
+                    newLevel = 1;
+                }
+                Application.LoadLevel(newLevel);
+                break;
         }
     }
 
@@ -108,11 +124,13 @@ public class CharacterManager : MonoBehaviour {
     {
         // TODO fancy stuff - fish collect
         this.fish++;
+        audioSource.PlayOneShot(collectFishClip);
         if (this.fish >= this.FishPerLife)
         {
             // TODO fancy stuff - extra live
             this.lives++;
             this.fish = 0;
+            audioSource.PlayOneShot(penguinClip);
             
             menuManager.UpdateLives(this.lives.ToString());
         }
@@ -127,6 +145,7 @@ public class CharacterManager : MonoBehaviour {
     {
         // TODO fancy stuff - friend collect
         this.friends++;
+        audioSource.PlayOneShot(penguinClip);
 
         menuManager.UpdateFriends(this.friends.ToString());
         // TODO fancy destroy?
