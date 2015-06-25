29b90fb7c68c150ada29b1fa5ec651ea11ee50bb
Juliusername <juliusodb@gmail.com>
2015-06-25 16:33:29 +0200

    Fontein, windturbine met button

--- PinguinPower/Assets/Scripts/Windturbine.cs
+++ PinguinPower/Assets/Scripts/Windturbine.cs
@@ -3,14 +3,27 @@ using System.Collections;
 
 public class Windturbine : MonoBehaviour {
 
+    public LeverCode Button;
     public float height = 10;
 
+    private GameObject windturbine;
     private ParticleSystem wind;
+    private bool playing;
 
 	// Use this for initialization
 	void Start () {
 
-        this.wind = this.transform.GetChild(0).GetComponent<ParticleSystem>();
+        this.windturbine = this.transform.GetChild(0).gameObject;
+        this.wind = this.windturbine.GetComponent<ParticleSystem>();
+
+        if (this.Button == null)
+        {
+            this.playing = false;
+        }
+        else
+        {
+            this.playing = true;
+        }
 	}
 	
 	// Update is called once per frame
@@ -19,5 +32,20 @@ public class Windturbine : MonoBehaviour {
         //Update Particle System
         this.wind.startSpeed = height;
         this.wind.startSize = height / 2;
+
+        if (this.Button != null)
+        {
+            bool enabled = this.Button.LeverEnabled();
+            if (enabled && !this.playing)
+            {
+                this.wind.Play();
+                this.playing = true;
+            }
+            else if (!enabled && this.playing)
+            {
+                this.wind.Stop();
+                this.playing = false;
+            }
+        }
 	}
 }
